75f4f6b3e9eb88f248bd9cf8048ecade9aeedd65
Wisnu Ginanjar Saputra <wisnu@zero-one-group.com>
2023-05-22 20:30:34 +0700

    ci: Setup ci automate build book

--- build.sh
+++
@@ -1,8 +0,0 @@
-npx honkit build && 
-mkdir _book/download/ && 
-npx honkit epub &&
-mv book.epub _book/download/quran.epub && 
-npx honkit mobi &&
-mv book.mobi _book/download/quran.mobi && 
-npx honkit pdf &&
-mv book.pdf _book/download/quran.pdf
