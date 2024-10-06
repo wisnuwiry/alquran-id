e8d25095d8d9b0fcecfb6ca5c175795268fd87c4
Wisnu G. Saputra <wisnuwiry@gmail.com>
2024-10-06 11:52:41 +0700

    feat: Add website logo

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Alert from "./Alert";
 import { CONFIG } from "@/utils/config";
+import Image from "next/image";
 
 const Footer = () => {
   return (
@@ -8,7 +9,10 @@ const Footer = () => {
       <div className="max-w-screen-xl mx-auto">
         {/* Content */}
         <div className="flex flex-col lg:flex-row gap-10 py-10 px-4 justify-between w-full">
-          <Link href="/"><h3 className="font-semibold text-xl">{CONFIG.APP_NAME}</h3></Link>
+          <Link href="/" className="flex flex-col gap-2 items-center">
+            <Image src="/logo.png" alt="Logo" width={60} height={60} />
+            <h3 className="font-semibold text-xl">{CONFIG.APP_NAME}</h3>
+          </Link>
           <Alert className="flex-1 max-w-2xl" title="Perhatian">
             <p className="text-sm text-neutral-600">Surah dan ayat yang kami tampilkan di laman ini berasal dari <a href="http://quran.com" target="_blank" rel="noopener noreferrer" className="text-blue-700">Quran.com</a>. Jika terjadi kesalahan atau menemukan kesalahan penampilan bisa menghubungi <a href="mailto:wisnuwiry@gmail.com" className="text-blue-700">saya</a>.</p>
           </Alert>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import { CONFIG } from "@/utils/config";
+import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="bg-gray-100 p-4">
       <div className="max-w-screen-xl mx-auto flex justify-between items-center">
-        <Link href="/">
+        <Link href="/" className="flex flex-row gap-2 items-center">
+          <Image src="/logo.png" alt="Logo" width={40} height={40}/>
           <h1 className="text-xl font-semibold">{CONFIG.APP_NAME}</h1>
         </Link>
       </div>
